d78b9f643a389d9c7cc1fe5b9aef24f0d542f7af
Kyle Nel <kyle@devnel.com>
2020-01-25 08:52:25 +0200

    initialise repo

---
+++ csv-edit.php
@@ -0,0 +1,14 @@
+<?php
+echo 'test';
+$file_handle = fopen('widgets.csv', 'r') ;
+$html = '<div>';
+
+while (!feof($file_handle)) {
+    $line_of_text = fgetcsv($file_handle, 1024);
+
+    //print_r($line_of_text);
+    $html .= $line_of_text[0].$line_of_text[1].$line_of_text[2]."<br>";
+}
+fclose($file_handle);
+
+print $html;
